b8c7eff88a73bbca18228dc42659c501a0711206
mason001 <mason001@hancock.pro>
2019-11-14 12:48:34 +0800

    Initial Commit

---
+++ MainArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainArchitecture
+//
+//  Created by Mason on 2019/11/14.
+//  Copyright © 2019 Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
